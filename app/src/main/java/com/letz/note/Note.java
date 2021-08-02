df5d9ac7089599933adc57536465dcebfa0c5494
letz <letz0703@gmail.com>
2021-08-02 22:14:57 +0900

    @Entity(tableName="")
    Entity Class 생성
    @Entity(tableName="note_table")

---
+++ app/src/main/java/com/letz/note/Note.java
@@ -0,0 +1,28 @@
+package com.letz.note;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String description;
+
+    public Note(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
