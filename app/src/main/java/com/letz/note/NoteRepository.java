709af55b9dc9441445e72db7595e042bac2e94f7
letz <letz0703@gmail.com>
2021-08-06 20:25:30 +0900

    Create NoteRepository.java
    async task

---
+++ app/src/main/java/com/letz/note/NoteRepository.java
@@ -0,0 +1,112 @@
+package com.letz.note;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteRepository
+{
+    private NoteDAO noteDAO;
+    private LiveData<List<Note>> notes;
+
+    public NoteRepository(Application application)
+    {
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDAO = database.noteDAO();
+        notes = noteDAO.getAllNotes();
+    }
+
+    public void insert(Note note)
+    {
+        new InsertNoteAsyncTask(noteDAO).execute(note);
+    }
+
+    public void update(Note note)
+    {
+        new UpdateNoteAsyncTask(noteDAO).execute(note);
+    }
+
+    public void delete(Note note)
+    {
+        new DeleteNoteAsyncTask(noteDAO).execute(note);
+    }
+
+    public LiveData<List<Note>> getAllNotes()
+    {
+        return notes;
+    }
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void>
+    {
+        private NoteDAO noteDAO;
+
+        private InsertNoteAsyncTask(NoteDAO noteDAO)
+        {
+            this.noteDAO = noteDAO;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes)
+        {
+            noteDAO.Insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void>
+    {
+        private NoteDAO noteDAO;
+
+        private InsertNoteAsyncTask(NoteDAO noteDAO)
+        {
+            this.noteDAO = noteDAO;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes)
+        {
+            noteDAO.Insert(notes[0]);
+            return null;
+        }
+    }
+
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void>
+    {
+        private NoteDAO noteDAO;
+
+        private UpdateNoteAsyncTask(NoteDAO noteDAO)
+        {
+            this.noteDAO = noteDAO;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes)
+        {
+            noteDAO.Update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void>
+    {
+        private NoteDAO noteDAO;
+
+        private  DeleteNoteAsyncTask(NoteDAO noteDAO)
+        {
+            this.noteDAO = noteDAO;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes)
+        {
+            noteDAO.Delete(notes[0]);
+            return null;
+        }
+    }
+
+
+}
